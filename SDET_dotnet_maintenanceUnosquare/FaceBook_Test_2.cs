4ba516e0107f92b56b1f44cf2393442077cff8a4
Luis Toscano <lmtm24@gmail.com>
2021-01-14 11:58:59 -0600

    Add project files.

---
+++ SDET_dotnet_maintenanceUnosquare/FaceBook_Test_2.cs
@@ -0,0 +1,93 @@
+﻿using java.lang;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+
+namespace lmtm24
+{
+    class FaceBook_Test_2
+    {
+       
+        #region Login Locators
+        By title = By.CssSelector("#reg_pages_msg");
+        By createNewAccountButton = By.CssSelector("form#u_0_a  a[role='button']");
+        By firstNameField = By.Name("firstname");
+        By lastNameField = By.Name("lastname");
+        By email = By.XPath("//input[@name='reg_email__']");
+        By reEmail = By.XPath("//input[@name='reg_email_confirmation__']");        
+        By pass = By.XPath("//input[@name='reg_passwd__']");
+        By month = By.XPath("//select[@id='month']");
+        By day = By.XPath("//select[@id='day']");
+        By year = By.XPath("//select[@id='year']");
+        By genderMale = By.CssSelector("span:nth-of-type(2) > ._58mt");
+        By signUpButton = By.Name("websubmit");
+
+
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            IWebDriver Browser;
+            IWebElement element;
+            Setup setup = new Setup();
+            FaceBook_Test_2 faceBook_Test = new FaceBook_Test_2();
+            Browser = setup.SetUpDriver();
+            Browser.Url = "https://www.facebook.com/";
+
+
+            element = Browser.FindElement(faceBook_Test.title);
+            setup.GetText(element);
+            setup.AreEquals("Create a Page for a celebrity, band or business.", setup.GetText(element));            
+
+            element = Browser.FindElement(faceBook_Test.createNewAccountButton);
+            setup.Click(element);
+
+            element = Browser.FindElement(faceBook_Test.firstNameField);
+            setup.SendText(element,"Luis");
+
+            element = Browser.FindElement(faceBook_Test.lastNameField);
+            setup.SendText(element, "Toscano");
+
+            element = Browser.FindElement(faceBook_Test.email);
+            setup.SendText(element, "luis.toscano@unoquare.com");
+
+            element = Browser.FindElement(faceBook_Test.reEmail);
+            setup.SendText(element, "luis.toscano@unoquare.com");
+
+            element = Browser.FindElement(faceBook_Test.pass);
+            setup.SendText(element, "Password#123");
+
+            element = Browser.FindElement(faceBook_Test.month);           
+            SelectElement selectMonth = new SelectElement(element);            
+            selectMonth.SelectByText("Nov");
+
+            element = Browser.FindElement(faceBook_Test.day);
+            SelectElement selectDay = new SelectElement(element);
+            selectDay.SelectByValue("24");
+
+            element = Browser.FindElement(faceBook_Test.year);
+            SelectElement selectYear = new SelectElement(element);
+            selectYear.SelectByValue("1980");
+
+            element = Browser.FindElement(faceBook_Test.genderMale);
+            setup.Click(element);
+
+            element = Browser.FindElement(faceBook_Test.signUpButton);
+            setup.Click(element);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ SDET_dotnet_maintenanceUnosquare/Setup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lmtm24
+{
+    class Setup
+    {
+
+        IWebDriver driver;
+        public IWebDriver SetUpDriver()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            return driver;
+        }
+
+        public void Click(IWebElement element)
+        {
+            element.Click();
+        }
+
+        public void SendText(IWebElement element, string value)
+        {
+            element.SendKeys(value);
+            Console.WriteLine(element + ":" + value);
+        }
+
+        public string GetText(IWebElement element)
+        {
+            string text = element.Text;
+            return text;
+        }
+
+        public void AreEquals(string expected, string actual)
+        {
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+    }
+}
